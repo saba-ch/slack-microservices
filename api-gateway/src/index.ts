97a92a7cf96bb1cbd3e0666034d3105c87c2e0aa
saba-coding <saba.chikhina@gmail.com>
2020-10-10 19:46:20 +0400

    chore: test push to dev

--- api-gateway/src/index.ts
+++ api-gateway/src/index.ts
@@ -7,7 +7,6 @@ if (!process.env.ORGANIZATION_URI) throw new Error('ORGANIZATION_URI is required
 if (!process.env.USER_URI) throw new Error('USER_URI is required')
 if (!process.env.CHANNEL_URI) throw new Error('CHANNEL_URI is required')
 
-
 const start = async () => {
   const gateway = new ApolloGateway({
     serviceList: [
